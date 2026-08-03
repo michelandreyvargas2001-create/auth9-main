602bd0b2e3a3f322d597ee2ccce98dce96ae033b
Michael Vargas <michelandreyvargas2001@gmail.com>
2026-08-03 17:07:35 -0500

    first commit

---
+++ src/users/controllers/users.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from '../services/users.service';
+
+@Controller('users')
+export class UsersController {
+
+    constructor(private readonly usersService: UsersService) { }
+
+    @Get()
+    findAll() {
+        return this.usersService.findAll(); // Sin DTOs
+    }
+
+}
